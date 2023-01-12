c3de0439ed5bcb4a979ef66a78115c59f6845b32
MR-Addict <2750417853@qq.com>
2023-01-13 02:38:37 +0800

    update loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,4 @@
-function CardSkeleton() {
+function SkeletonCard() {
   return (
     <div className='flex flex-col items-start gap-3 py-7 px-5 rounded-md border border-gray-300 animate-pulse'>
       <div className='h-5 w-2/3 bg-gray-200 rounded-md dark:bg-gray-700'></div>
@@ -10,12 +10,25 @@ function CardSkeleton() {
 export default function Loading() {
   return (
     <div className='frame w-full flex flex-col gap-3'>
-      <div className='h-5 w-24 bg-gray-200 rounded-md dark:bg-gray-700 animate-pulse'></div>
-      <div className='grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        <CardSkeleton />
-        <CardSkeleton />
-        <CardSkeleton />
-        <CardSkeleton />
+      <div className='flex flex-col gap-10'>
+        <div className='flex flex-col gap-3'>
+          <div className='h-5 w-24 bg-gray-200 rounded-md dark:bg-gray-700 animate-pulse'></div>
+          <div className='grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+          </div>
+        </div>
+        <div className='hidden md:flex flex-col gap-3'>
+          <div className='h-5 w-24 bg-gray-200 rounded-md dark:bg-gray-700 animate-pulse'></div>
+          <div className='grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+          </div>
+        </div>
       </div>
     </div>
   );
