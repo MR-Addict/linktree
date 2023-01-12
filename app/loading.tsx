5135159c0557c5fedee2cc3f2070d2569eb532cf
MR-Addict <2750417853@qq.com>
2023-01-12 17:53:19 +0800

    update loading skeleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 function CardSkeleton() {
   return (
-    <div className='animate-pulse flex flex-col items-start gap-3 py-7 px-5 rounded-md border border-gray-300'>
+    <div className='flex flex-col items-start gap-3 py-7 px-5 rounded-md border border-gray-300 animate-pulse'>
       <div className='h-5 w-2/3 bg-gray-200 rounded-md dark:bg-gray-700'></div>
       <div className='h-32 w-full bg-gray-200 rounded-md dark:bg-gray-700'></div>
     </div>
@@ -9,11 +9,14 @@ function CardSkeleton() {
 
 export default function Loading() {
   return (
-    <div className='frame w-full items-center justify-center grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      <CardSkeleton />
-      <CardSkeleton />
-      <CardSkeleton />
-      <CardSkeleton />
+    <div className='frame w-full flex flex-col gap-3'>
+      <div className='h-5 w-16 bg-gray-200 rounded-md dark:bg-gray-700 animate-pulse'></div>
+      <div className='grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </div>
     </div>
   );
 }
