4ecd09a6b592212786b1d136229d4fb067e34f8d
MR-Addict <2750417853@qq.com>
2023-01-14 12:13:27 +0800

    change popup style

--- app/components/Popup/Popup.tsx
+++ app/components/Popup/Popup.tsx
@@ -21,34 +21,40 @@ export default function Popup({
 
   return (
     <div
-      className={`z-10 fixed -top-10 left-[50%] translate-x-[-50%] duration-500 ${
-        isPopup ? "translate-y-32" : "translate-y-0"
+      className={`w-full z-10 fixed flex flex-col items-center -top-20 left-[50%] translate-x-[-50%] duration-500 ${
+        isPopup ? "translate-y-44" : "translate-y-0"
       }`}
     >
       {popupData.status ? (
-        <div className='bg-green-100 border border-green-900 px-4 py-1 rounded-sm flex flex-row items-center gap-2 w-fit'>
-          <h1 className='font-semibold whitespace-nowrap text-green-900'>{popupData.message}</h1>
+        <div className='w-full max-w-xs flex flex-row items-center px-2 py-1 rounded-sm bg-green-100 border border-green-900 border-l-[5px] border-l-green-900'>
+          <div className='flex flex-col flex-1'>
+            <h1 className='text-green-900 font-bold text-xl'>Success</h1>
+            <div className='flex flex-row items-center justify-between gap-2'>
+              <h1 className='font-semibold whitespace-nowrap text-green-900'>{popupData.message}</h1>
+            </div>
+          </div>
           <button
             type='button'
-            onClick={() => {
-              setIsPopup(false);
-            }}
-            className='bg-green-900 text-white p-1 rounded-full'
+            onClick={() => setIsPopup(false)}
+            className='bg-green-900 text-white p-1 rounded-full w-fit h-fit'
           >
-            <RxCross1 size={8} />
+            <RxCross1 size={10} />
           </button>
         </div>
       ) : (
-        <div className='bg-red-100 border border-red-900 px-4 py-1 rounded-sm flex flex-row items-center gap-2 w-fit'>
-          <h1 className='font-semibold whitespace-nowrap text-red-900'>{popupData.message}</h1>
+        <div className='w-full max-w-xs flex flex-row items-center px-2 py-1 rounded-sm bg-red-100 border border-red-900 border-l-[5px] border-l-red-900'>
+          <div className='flex flex-col flex-1'>
+            <h1 className='text-red-900 font-bold text-xl'>Error</h1>
+            <div className='flex flex-row items-center justify-between gap-2'>
+              <h1 className='font-semibold whitespace-nowrap text-red-900'>{popupData.message}</h1>
+            </div>
+          </div>
           <button
             type='button'
-            onClick={() => {
-              setIsPopup(false);
-            }}
-            className='bg-red-900 text-white p-1 rounded-full'
+            onClick={() => setIsPopup(false)}
+            className='bg-red-900 text-white p-1 rounded-full w-fit h-fit'
           >
-            <RxCross1 size={8} />
+            <RxCross1 size={10} />
           </button>
         </div>
       )}
